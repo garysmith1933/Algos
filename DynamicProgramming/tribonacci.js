f331c6cd4eb62e63950ff5264cbea39f2322f121
Gary Smith <garysmith193358@gmail.com>
2022-11-22 08:01:31 -0500

    completed tribonacci

---
+++ DynamicProgramming/tribonacci.js
@@ -0,0 +1,13 @@
+const tribonacci = (n, memo = {}) => {
+  if(n === 1 || n === 0) return 0
+  if(n === 2) return 1;
+  
+  if(n in memo) return memo[n];
+  
+  memo[n] = tribonacci(n-1, memo) + tribonacci(n-2, memo) + tribonacci(n-3, memo)
+  
+  return memo[n]
+  
+  //Time: O(n)
+  //Space: O(n)
+};
