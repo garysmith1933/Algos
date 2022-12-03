7847d7c7e4cf4b7c55e3116e01606ceba7c19ac2
Gary Smith <garysmith193358@gmail.com>
2022-12-02 19:01:20 -0500

    updated top k frequent elements

--- ArrayAndStrings/topKFrequentElements.js
+++ ArrayAndStrings/topKFrequentElements.js
@@ -17,13 +17,18 @@ var topKFrequent = function(nums, k) {
     }
   }
   
-    for (let i = bucket.length - 1; i >= 0; i--) {
-      if (bucket[i]) result.push(...bucket[i])
-      if(result.length === k) break;
-    }
+  //start backwards and add the values to the results array until the length of it is equal to k.
+  for (let i = bucket.length - 1; i >= 0; i--) {
+    if (bucket[i]) result.push(...bucket[i])
+    if(result.length === k) break;
+  }
 
   return result;
 };
 
 //Time: O(N) n is length of nums array.
 //Space: O(N)
+
+//The goal of this is to create an array that represents the # times a number shows up, with the values of the array being the number(s) that shows up x amount of times.
+
+//example: a bucket that represents numbers that have appeared 3 times in the array would only have 1 in it as it is the only number to so.
