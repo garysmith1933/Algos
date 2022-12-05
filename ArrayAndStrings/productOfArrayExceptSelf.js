7e1906f45544bb0153192a37b82d45f77efe7e22
Gary Smith <garysmith193358@gmail.com>
2022-12-05 07:36:24 -0500

    completed product of array except self

---
+++ ArrayAndStrings/productOfArrayExceptSelf.js
@@ -0,0 +1,16 @@
+var productExceptSelf = function(nums, i = 0, answers = []) {
+  if (nums[i] === undefined) return answers;
+    let product = 1;
+    for ( let j = 0; j < nums.length; j++) {
+      if (i === j) continue;
+
+      product *= nums[j];
+      if(product === 0) break;
+    }
+
+    answers.push(product);
+    return productExceptSelf(nums, i + 1, answers);
+};
+
+//Time: O(N) n is length of nums array
+//Space: O(N) recursion
