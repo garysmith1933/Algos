64dfa42cda84681c80cbb063e77296a5dd7891b7
Gary Smith <garysmith193358@gmail.com>
2022-12-05 07:38:52 -0500

    modified formatting

--- ArrayAndStrings/productOfArrayExceptSelf.js
+++ ArrayAndStrings/productOfArrayExceptSelf.js
@@ -1,16 +1,14 @@
 var productExceptSelf = function(nums, i = 0, answers = []) {
   if (nums[i] === undefined) return answers;
-    let product = 1;
-    for ( let j = 0; j < nums.length; j++) {
-      if (i === j) continue;
 
-      product *= nums[j];
-      if(product === 0) break;
-    }
+  let product = 1;
 
-    answers.push(product);
-    return productExceptSelf(nums, i + 1, answers);
+  for ( let j = 0; j < nums.length; j++) {
+    if (i === j) continue;
+    product *= nums[j];
+    if(product === 0) break;
+  }
+
+  answers.push(product);
+  return productExceptSelf(nums, i + 1, answers);
 };
-
-//Time: O(N) n is length of nums array
-//Space: O(N) recursion
