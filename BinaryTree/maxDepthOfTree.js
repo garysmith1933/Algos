b8ce254beb3901667d2765d3ef7c556f71f7daa2
Gary Smith <garysmith193358@gmail.com>
2022-12-16 10:48:56 -0500

    added depth first solution to maxDepthOfTree

--- BinaryTree/maxDepthOfTree.js
+++ BinaryTree/maxDepthOfTree.js
@@ -1,16 +1,42 @@
+ //Breadth first
+// var maxDepth = function(root) {
+//     if (!root) return 0;
+//     const queue = [{node: root, level: 1}]
+
+//     while (queue.length > 0) {
+//       const { node, level } = queue.shift()
+
+//       if(node.left) queue.push({node: node.left, level: level + 1});
+//       if(node.right) queue.push({node: node.right, level: level + 1});
+
+//       if (queue.length === 0) return level;
+//     }
+// };
+
+// //Time: O(N) n is # of nodes in tree;
+// //Space: O(N)
+
+
+//Depth First
+
 var maxDepth = function(root) {
-    if (!root) return 0;
-    const queue = [{node: root, level: 1}]
+  if (!root) return 0;
 
-    while (queue.length > 0) {
-      const { node, level } = queue.shift()
+  let maxLevel = 1;
 
-      if(node.left) queue.push({node: node.left, level: level + 1});
-      if(node.right) queue.push({node: node.right, level: level + 1});
+  const stack = [{node: root, level: 1}]
 
-      if (queue.length === 0) return level;
-    }
+  while (stack.length > 0) {
+    const { node, level } = stack.pop();
+
+    maxLevel = Math.max(maxLevel, level)
+
+    if(node.left) stack.push({node: node.left, level: level + 1});
+    if(node.right) stack.push({node: node.right, level: level + 1});
+  }
+
+  return maxLevel;
 };
 
-//Time: O(N) n is # of nodes in tree;
+//Time: O(N)
 //Space: O(N)
