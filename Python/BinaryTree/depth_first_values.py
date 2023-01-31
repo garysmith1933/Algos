48221fd16ef3afbd27848ff0a9e2b6de6b021d1e
Gary Smith <garysmith193358@gmail.com>
2023-01-31 15:43:05 -0500

    completed depth first nodes

---
+++ Python/BinaryTree/depth_first_values.py
@@ -0,0 +1,30 @@
+def depth_first_values(root):
+  if not root: 
+    return []
+  
+  values = []
+  stack = [root]
+  
+  while stack:
+    current = stack.pop()
+    values.append(current.val)
+    
+    if current.right is not None: stack.append(current.right)
+    if current.left is not None: stack.append(current.left)
+    
+  return values
+
+# Recurison
+
+# def depth_first_values(root, values=[]):
+#   if not root:
+#     return values
+  
+#   values.append(root.val)
+#   depth_first_values(root.left, values)
+#   depth_first_values(root.right, values)
+  
+#   return values
+
+ # Time: O(N)
+  # Space: O(N)
