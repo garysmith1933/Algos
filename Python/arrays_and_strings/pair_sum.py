23448bf7a4e92303fc865b95be857517ab0e3f2d
Gary Smith <garysmith193358@gmail.com>
2023-02-02 20:20:37 -0500

    added optimized solution to pair sum

--- Python/arrays_and_strings/pair_sum.py
+++ Python/arrays_and_strings/pair_sum.py
@@ -17,3 +17,24 @@ def pair_sum(numbers, target_sum):
   
   #Time O(N*2)
   # Space (O(1))
+
+  def pair_sum2(numbers, target_sum):
+  # dict
+    prev_nums = {}
+    # iterate over numbers
+    for idx in range(len(numbers)):
+      current_num = numbers[idx]
+      # other half = target - value of current numbers
+      other_half = target_sum - current_num
+      # if its in our dict
+      if other_half in prev_nums:
+        #RETURN TUPLE 
+        return (prev_nums[other_half], idx)
+    
+      #else
+      else:
+        # add to dictionary with key as other half, value as current idx
+        prev_nums[current_num] = idx
+      
+  #Time O(N)
+  # Space (O(N))
