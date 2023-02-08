bf8520c19d1e75ca37799cccabe3796a46030811
Gary Smith <garysmith193358@gmail.com>
2023-02-08 15:39:23 -0500

    completed five sort

---
+++ Python/arrays_and_strings/five_sort.py
@@ -0,0 +1,19 @@
+def five_sort(nums):
+  i = 0 
+  j = len(nums) - 1
+ 
+  while i < j:
+    if nums[j] == 5:
+      j -= 1
+      
+    elif nums[i] == 5:
+      nums[i], nums[j] = nums[j], nums[i]
+      i += 1
+ 
+    else:
+      i += 1
+      
+  return nums
+
+#Time: O(N) n = size of list
+#Space: O(1)
