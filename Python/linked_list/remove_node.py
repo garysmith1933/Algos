00854a4819b2e60f414df39db3c918caa6e2ff1e
Gary Smith <garysmith193358@gmail.com>
2023-02-10 09:58:40 -0500

    completed remove node

---
+++ Python/linked_list/remove_node.py
@@ -0,0 +1,13 @@
+def remove_node(head, target_val):
+  if head is None:
+    return None
+
+  if head.val == target_val:
+    return head.next
+
+  head.next = remove_node(head.next, target_val)
+  return head
+
+#Time: O(N)
+#Space: O(1)
+  
